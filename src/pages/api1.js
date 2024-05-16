5f69ef2c3b901674172233c5793c37bbaf10b098
MateysCaptain <piracycoin@gmail.com>
2024-05-16 23:18:52 +0530

    Update api1.js

--- src/pages/api1.js
+++ src/pages/api1.js
@@ -6,7 +6,7 @@ const searchMovies = async () => {
 	const response = await axios.get("https://api.themoviedb.org/3/search/multi?query=kaabil&include_adult=false&language=en-US&page=1", {
 		headers: {
 		    accept: 'application/json',
-		    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQzOGRlNzRlYTUwZDRkNDE5Mzk0OTM0OTczYTA0MCIsInN1YiI6IjY1OWUyYzkzOGU4ZDMwMDE0YzIwMjExYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.D0rTluAv1sY7ne6WtCShIY8TfJIsx7HQ6FajTpbXC-w'
+		    Authorization: 'Bearer your-own-api-key'
 		}
 	});
 
